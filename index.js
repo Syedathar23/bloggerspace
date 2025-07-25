6c5d0ed79a44fb7babb4310269114aad6802899f
Syedathar23 <nitaspirant81@gmail.com>
2025-07-25 12:07:33 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+let post = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    res.render("home.ejs",{
+        
+        posts:post,
+    });
+});
+
+app.get("/create",(req,res)=>{
+    res.render("creation.ejs");
+});
+
+app.post("/created",(req,res)=>{
+     const newpost ={
+        post : req.body["postBody"],
+        title : req.body["postTitle"]
+    };
+    post.push(newpost);
+    
+    res.redirect("/");
+    
+})
+
+app.listen(port,()=>{
+    console.log(`Server is live at port ${port}`);
+});
