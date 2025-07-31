68476caba850fa8e1b588c5ac02b8da45ca96320
syed Athar <150650323+Syedathar23@users.noreply.github.com>
2025-07-31 12:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 let post = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
